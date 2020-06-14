f868bf8f966a8eff733c6e4666c5740988856134
Kayk Mascarenhas <ksm.kayk@outlook.com>
2020-06-14 16:13:12 -0300

    get /empresas

--- backend-node/src/routes.js
+++ backend-node/src/routes.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
+const empresaController = require("../controller/empresaController")
 
 const routes = Router();
+routes.get("/empresas", empresaController.index)
 
 module.exports = routes;
